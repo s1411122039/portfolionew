fef85b206a50eeb6b5b29db485b4fe76c1bd95f5
s1411122039 <abc0979775465@gmail.com>
2024-12-07 12:22:13 +0800

    1207

--- server.js
+++ server.js
@@ -2,22 +2,26 @@
 //install web server package: express >npm install express
 var express = require("express");
 var server = express();
+var bodyParser = require("body-parser");
 
 //web root
 server.use(express.static(__dirname+"/AgencyProject"));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded());
+
 
 var DB = require("nedb-promises");
 var ProfolioDB = DB.create(__dirname+"/profolio.db");
-
+var ContactDB = DB.create(__dirname+"/contact.db");
  
-ProfolioDB.insert([
-    { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-    { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-    { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
-    { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-    { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
- ])
+// ProfolioDB.insert([
+//     { modal: "#portfolioModal1", imgSrc: "modalroundicons.png", heading: "Round Icons", text: "Graphic Design" },
+//     { modal: "#portfolioModal2", imgSrc: "startup-framework.png", heading: "Startup Framework", text: "Website Design" },
+//     { modal: "#portfolioModal3", imgSrc: "treehouse.png", heading: "Treehouse", text: "Website Design" },
+//     { modal: "#portfolioModal1", imgSrc: "roundicons.png", heading: "Round Icons", text: "Graphic Design" },
+//     { modal: "#portfolioModal2", imgSrc: "startup-framework.png", heading: "Startup Framework", text: "Website Design" },
+//     { modal: "#portfolioModal3", imgSrc: "treehouse.png", heading: "Treehouse", text: "Website Design" }
+// ])
 
 server.get("/services", (req, res)=>{
     //DB find
@@ -37,10 +41,12 @@ server.get("/profolio", (req,res)=>{
             res.send("Error!");
         }
       })
-     
-
 })
 
+server.post("/contact_me", (req,res)=>{
+     ContactDB.insert(req.body);
+     res.redirect("/#contact");
+})
 
 server.listen(80, ()=>{
     console.log("Server is running at port 80.");
