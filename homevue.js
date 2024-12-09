59e5a40d65efde80793c68b23185b2121014caec
s1411122039 <abc0979775465@gmail.com>
2024-12-09 19:29:26 +0800

    1209

--- homevue.js
+++ homevue.js
@@ -1,3 +1,39 @@
+
+const app = Vue.createApp({
+    data() {
+      return {
+        activeIndex: 0, // 用來記錄當前顯示的圖片索引
+        carouselItems: [
+          {
+            title: "Painting",
+            description: "We had such a great time in LA!",
+            imgSrc: "picture/monster.jpg",
+            alt: "Painting"
+          },
+          {
+            title: "Photography",
+            description: "We had such a great time in LA!",
+            imgSrc: "picture/IMG_8383.jpg",
+            alt: "Photography"
+          },
+          {
+            title: "Game production",
+            description: "We had such a great time in LA!",
+            imgSrc: "picture/gamedevelop.jpg",
+            alt: "Game production"
+          },
+          {
+            title: "Animation",
+            description: "We had such a great time in LA!",
+            imgSrc: "picture/gamedevelop.jpg",
+            alt: "Animation"
+          }
+        ]
+      };
+    },
+});
+app.mount("#carousel");
+
 const card = Vue.createApp({
     data() {
       return {
